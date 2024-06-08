fe91947a12e4d93a3887136fc31985354776185d
hiengyen <ngyentrunghieu742003@gmail.com>
2024-06-08 10:59:28 +0700

    update query.sql

--- query.sql
+++ query.sql
@@ -1,146 +1,123 @@
+create type AccountRoll as enum('ADMIN', 'USER');
+create type InvoiceStatus as enum('NEW', 'SHIPPING', 'DONE', 'ABORT');
+create type PaymentMethod as enum('COD', 'BANKING');
 
-create type permissions as enum('admin', 'user');
-
-create table users (
-  user_id serial primary key,
-  -- name  not null,
-  email text not null,
-  passwd text not null,
-  phone_number text,
-  avt text,
-  status boolean not null,
-  is_banned boolean default(false),
-  private_key text not null,
-  public_key text not null,
-  refresh_token text not null,
-  refresh_token_used text[],
-  role permissions not null,
-  created_at timestamp(3) default current_timestamp,
-  editted_at timestamp(3),
-
+create table Provider (
+  providerID text primary key,
+  providerName text not null
 );
 
-
-create table color (
-  color_id text primary key,
-  name text not null
+create table Category (
+  categoryID text primary key,
+  categoryName text not null
 );
 
-create table storage (
-  storage_id text primary key,
-  src text not null
-);
-
-create table branch (
-  branch_id text primary key,
-  name text not null
-);
-
-create table attribute_type (
-  type_id text primary key,
-  atr_type text not null
-);
-
-create table category (
-  cate_id text primary key,
-  title text not null
-);
-
-create table store (
-  store_id text primary key,
-  name text not null,
+create table Store (
+  storeID text primary key,
+  storeName text not null,
   description text,
   address text,
-  phone_number text,
+  phoneNumber text,
   email text,
-  fb_url text,
   bannerUrls text[]
 );
 
-create table product (
-  product_id text primary key,
-  name text not null,
-  description text,
-  height text not null,
-  weight text not null,
-  len text not null,
-  width text not null,
-  gurantee text not null,
-  category_id text not null references category(cate_id),
-  branch_id text not null references branch(branch_id)
+create table AttributeType (
+  typeID text primary key,
+  typeValue text not null
 );
 
-create table product_item (
-  item_id text primary key,
+create table Account (
+  accountID text primary key,
+  accountName text not null,
+  avt text,
+  phoneNumber text,
+  createAt timestamp(3),
+  editedAt timestamp(3),
+  isBanned boolean default(false),
+  email text not null,
+  passwd text not null,
+  refreshToken text not null,
+  refreshTokenUsed text[],
+  roll AccountRoll not null
+);
+
+create table Product (
+  productID text primary key,
+  productName text not null,
+  description text,
+  height float not null,
+  weight float not null,
+  len float not null,
+  width float not null,
+  gurantee float not null,
+  categoryID text not null references Category(categoryID),
+  providerID text not null references Provider(providerID)
+);
+
+create table ProductItem (
+  itemID text primary key,
   thump text not null,
-  status boolean not null default(true),
   quantity integer not null,
   price float not null,
-  is_prior boolean not null,
-  product_code text,
-  discount float,
-  create_at timestamp(3) default current_timestamp,
-  edittedAt timestamp(3),
-  product_id text not null references product(product_id),
-  color_id text not null references color(color_id),
-  storage_id text references storage(storage_id)
+  productCode text unique,
+  discount float default(0),
+  colorName text not null,
+  storageName text,
+  productID text not null references Product(productID)
 );
 
-create table attribute_option (
-  option_id text primary key,
-  option_value text not null,
-  attribute_type_id text not null references attribute_type(type_id)
+create table AttributeOption (
+  optionID text primary key,
+  optionValue text not null,
+  typeID text not null references AttributeType(typeID)
 );
 
-create table product_attribute (
-  product_id text not null references product(product_id),
-  attribute_option_id text not null references attribute_option(option_id)
+create table ProductAttribute (
+  productID text not null references Product(productID),
+  optionID text not null references AttributeOption(optionID)
 );
 
-
-create table product_image (
-  image_id text primary key,
+create table ItemImage (
+  imageID text primary key,
   src text not null,
-  item_id text not null references product_item(item_id)
+  itemID text not null references ProductItem(itemID)
 );
 
-create table review (
-  review_id text primary key,
-  review_content text not null,
+create table Review (
+  reviewID text primary key,
+  reviewContent text not null,
   rating integer default(5),
-  created_at timestamp(3) default current_timestamp,
-  product_id text not null references product(product_id),
-  user_id text not null references users(user_id)
+  createdAt timestamp(3) default current_timestamp,
+  productID text not null references Product(productID),
+  accountID text not null references Account(accountID)
 );
- 
-create type orderstatus as enum('NEW', 'SHIPPING', 'DONE', 'ABORT');
-create type paymentmethod as enum('COD', 'BANKING');
 
-create table orders (
-  order_id text primary key,
-  status orderstatus not null,
-  payment paymentmethod not null,
+create table Invoice (
+  invoiceID text primary key,
+  status InvoiceStatus not null,
+  payment PaymentMethod not null,
   city text not null,
   ward text not null,
   province text not null,
-  phone_number text not null,
-  detail_address text not null,
-  created_at timestamp(3) default current_timestamp,
-  editted_at timestamp(3),
-  user_id text not null references users(user_id)
+  phoneNumber text not null,
+  detailAddress text not null,
+  createdAt timestamp(3) default current_timestamp,
+  accountID text not null references Account(accountID)
 );
 
-create table order_product (
+create table InvoiceProduct (
   discount float,
   price float not null,
+  productName text not null,
   quantity integer not null,
-  order_id text references orders(order_id),
-  product_id text references product(product_id)
+  invoiceID text references Invoice(invoiceID),
+  productID text references Product(productID)
 );
 
-create table slide_show (
+create table SlideShow (
   slide_id text primary key,
   url text not null,
   alt text not null,
-  store_id text not null references store(store_id)
+  storeID text not null references Store(storeID)
 );
