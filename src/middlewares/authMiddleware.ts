4573aa9e81a25f57d686d4ae61866abe64408c82
hiengyen <ngyentrunghieu742003@gmail.com>
2024-06-13 22:13:01 +0700

    update authMiddleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,6 +3,7 @@ import { AuthFailureError } from '../utils/errorResponse'
 import { JwtUtil } from '../utils/jwtUtil'
 import { AT_KEY } from '../controllers/userController'
 import logger from '../utils/logger'
+import { StatusCode } from '../utils/httpStatusCodes'
 //Get and authorized accessToken receive from FE
 const isAuthorized = async (
   req: Request,
@@ -25,7 +26,13 @@ const isAuthorized = async (
     logger.info(accessTokenDecoded)
     // req.jwtDecoded = accessTokenDecoded
     next()
-  } catch (error) {}
+  } catch (error: any) {
+    if (error.message?.includes('jwt expired')) {
+      throw new AuthFailureError('Need use refresh token', StatusCode.GONE)
+    }
+
+    throw new AuthFailureError('Unauthorized! Please login...')
+  }
 }
 
 export const authMiddleware = {
